mcb30/puppet-dev

commit 7eb50ed0925895dca033370d3c82e5ca93b82c6f

deps: Use patched version of puppet-r10k
The puppet-r10k module's webhook currently fails to support both IPv6
and LetsEncrypt certificates (or any other certificates not directly
issued by a trusted CA root certificate).

Use our patched version until the fixes are upstreamed.

Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

Puppetfile
@@ -1,6 +1,8 @@
 mod 'camptocamp-systemd', :latest
 mod 'puppet-letsencrypt', :latest
-mod 'puppet-r10k', :latest
+mod 'puppet-r10k',
+    :git => 'https://github.com/unipartdigital/puppet-r10k',
+    :branch => 'fixes'
 mod 'puppet-ssh_keygen', :latest
 mod 'puppetlabs-git', :latest
 mod 'puppetlabs-inifile', :latest

commit fa61ce1a2ebf50083aec255fa7fc2c6c6530dada

deps: Switch back to upstream puppet-r10k
Commit 7eb50ed ("deps: Use patched version of puppet-r10k") introduced
a patched version of r10k to work around upstream bugs.  Our upstream
fixes were merged and included in the upstream v8.1.0 release, so we
can switch back to using the vanilla upstream.

Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

Puppetfile
@@ -1,9 +1,7 @@
 mod 'camptocamp-systemd', :latest
 mod 'puppet-hiera', :latest
 mod 'puppet-letsencrypt', :latest
-mod 'puppet-r10k',
-    :git => 'https://github.com/unipartdigital/puppet-r10k',
-    :branch => 'fixes'
+mod 'puppet-r10k', :latest
 mod 'puppet-selinux', :latest
 mod 'puppet-ssh_keygen', :latest
 mod 'puppetlabs-apache', :latest